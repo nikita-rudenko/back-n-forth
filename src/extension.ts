f6ef959c8a5cd28c2712e7be62d2ff5147aa1b88
Nick Rudenko <rdnkta@gmail.com>
2019-10-13 13:51:01 +0300

    Add ability to change the icon style

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,37 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	const navigateBack = vscode.commands.registerCommand('extension.showBackBtn', () => {
-		vscode.commands.executeCommand('workbench.action.navigateBack');
-	});
+function setup(context: vscode.ExtensionContext) {
+  const iconStyle = vscode.workspace
+    .getConfiguration('back-n-forth')
+    .get('iconStyle');
 
-	const navigateForward = vscode.commands.registerCommand('extension.showForwardBtn', () => {
-		vscode.commands.executeCommand('workbench.action.navigateForward');
-	});
+  const navigateBack = vscode.commands.registerCommand(
+    `extension.${iconStyle}BackBtn`,
+    () => {
+      vscode.commands.executeCommand('workbench.action.navigateBack');
+    }
+  );
 
-	context.subscriptions.push(navigateBack, navigateForward);
+  const navigateForward = vscode.commands.registerCommand(
+    `extension.${iconStyle}ForwardBtn`,
+    () => {
+      vscode.commands.executeCommand('workbench.action.navigateForward');
+    }
+  );
+
+  context.subscriptions.push(navigateBack, navigateForward);
 }
 
-export function deactivate() { }
+export function activate(context: vscode.ExtensionContext) {
+  // Listen for icon style change
+  vscode.workspace.onDidChangeConfiguration(event => {
+    let affected = event.affectsConfiguration('back-n-forth.iconStyle');
+    if (affected) {
+      setup(context);
+    }
+  });
+
+  setup(context);
+}
+
+export function deactivate() {}
